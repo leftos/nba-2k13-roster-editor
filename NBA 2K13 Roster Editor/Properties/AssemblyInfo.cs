a06d156c1bd398d22bcec610178276ec2b1b4b08
Leftos <leftos@gmail.com>
2013-03-02 13:31:46 +0200

    (v0.22.4.2) Update version numbers

--- NBA 2K13 Roster Editor/Properties/AssemblyInfo.cs
+++ NBA 2K13 Roster Editor/Properties/AssemblyInfo.cs
@@ -76,4 +76,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.22.4.1")]
+[assembly: AssemblyVersion("0.22.4.2")]
